ac76b47e93b0b909daad89689f3f5e963b84f0ea
Ryan Gabriel Molina <ryangabrielpmolina@gmail.com>
2020-04-11 09:54:48 +0000

    Updates

--- setup.sh
+++ setup.sh
@@ -1,12 +1,26 @@
 sudo apt-get update
 sudo apt-get install git
+sudo apt-get install fuse
+sudo modprobe fuse
+sudo usermod -a -G fuse $USER
 
 echo "[INSTALL] Vim + Neovim"
+rm -rf ~/.config/
+mkdir ~/.config/
+mkdir ~/.config/nvim/
+rm -rf ~/dotfiles
+
+mkdir ~/AppImage/
+mkdir ~/AppImage/neovim-image/
 curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage > ~/AppImage/neovim-image/nvim.appimage
 chmod +x ~/AppImage/neovim-image/nvim.appimage
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mkdir ~/.config/nvim/
-git clone https://github.com/ryangmolina/dotfiles ~/
+~/AppImage/neovim-image/nvim.appimage
+ln -s ~/AppImage/neovim-image/nvim.appimage nvim
+sudo cp ~/AppImage/neovim-image/nvim.appimage /usr/bin/nvim
+export PATH=~/AppImage/neovim-image/nvim.appimage/:$PATH
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.config/nvim/bundle/Vundle.vim
+git clone https://github.com/ryangmolina/dotfiles ~/dotfiles
 cp ~/dotfiles/init.vim ~/.config/nvim/init.vim
 cp ~/dotfiles/.vimrc ~/.vimrc
 nvim +PluginInstall +qall
@@ -18,6 +32,10 @@ sudo apt-get install docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
 docker --version
+
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
 echo "[DONE] Docker"
 
 echo "[INSTALL] Nginx"
